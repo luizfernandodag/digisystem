5ad0c41efeb9ab6717a89b3921ec7a84e1c3fc24
luizfernandodag <luizfernandodag@gmail.com>
2023-11-30 22:30:04 -0300

    primeiro commit

---
+++ src/test/java/com/digisystem/luiz/LuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digisystem.luiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
